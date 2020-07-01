60a613ada60dcc82c78df6503d4a2df6d44d885e
Marija Soldatovic <soldatovicmarija00@gmail.com>
2020-07-01 15:07:13 +0100

    Complete ToDos

--- src/components/views/Index/components/ToDos.vue
+++ src/components/views/Index/components/ToDos.vue
@@ -2,14 +2,22 @@
   <div class="to-do-list">
       <h3>To Do's</h3>
       <div class="list">
-            <slot></slot>
+            <ToDo v-for="todo in todos" :key="todo.id" :todo="todo.todo" :id="todo.id" :isCompleted="todo.completed" @check-todo="$emit('check-todo', todo.id)"></ToDo>
       </div>
   </div>
 </template>
 
 <script>
+import ToDo from "./ToDo"
 export default {
     name:"ToDos",
+    components: {ToDo},
+    props: {
+        todos:undefined,
+    },
+    methods:{
+
+    },
 }
 </script>
 
